82c6c771981d71e6b3f3a1afed939b9aed268c47
anwarvic <mohamedanwarvic@gmail.com>
2019-02-25 11:58:48 +0200

    adding a script to examine hdf5 files

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import os
 import subprocess
+import h5py
+import numpy as np
 
+
+
+
+def explore_file(filepath):
+    """
+    This function is used to explore any hdf5 file.
+    """
+    h5 = h5py.File(filepath, mode="r")
+    print(h5.keys())
+    for key, value in h5.items():
+        print("Key:", key)
+        print("Value Type:", value)
+        value = np.array(value)
+        print("Value Shape:", value.shape)
+        print("Value:", value)
+        print("="*25)
 
 
 def preprocessAudioFile(inwav, outwav, sample_rate, n_channels, bit=16):
@@ -10,7 +28,7 @@ def preprocessAudioFile(inwav, outwav, sample_rate, n_channels, bit=16):
     and that's because SideKit has problems for other precisions.
     And all audio files need to be the same criteria.
     NOTE:
-    wave.py only reads wav files in PCM format, so make sure that's the case!!
+    It only reads wav files in PCM format, so make sure that's the case!!
     """
     parent, _ = os.path.split(outwav)
     if not os.path.exists(parent):
