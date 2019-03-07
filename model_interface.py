172465523f58c98617678fe121fe10beaf760074
anwarvic <mohamedanwarvic@gmail.com>
2019-03-07 08:11:04 +0200

    minor changes

--- model_interface.py
+++ model_interface.py
@@ -1,5 +1,6 @@
 import os
 import sidekit
+import numpy as np
 from multiprocessing import cpu_count
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -49,3 +50,57 @@ class SidekitModel():
     
     def plotDETcurve(self):
         pass
+    
+    def getAccuracy(self, speakers, test_files, scores, mode=2, threshold=0):
+        """
+        This private method is used to get the accuracy of a model
+        given five pieces of information:
+        -> speakers: list of speakers
+        -> test_files: list of filenames that used to evaluate model
+        -> scores: score numpy matrix obtained by the model
+        -> mode: which is one of these values [0, 1, 2] where:
+            -> 0: means get verification accuracy.
+            -> 1: means get recognition accuracy.
+            -> 2: means get both accuracy, verification and recognition.
+        -> threshold: the value above which we will consider the verification
+                is done correctly. In other words, if the score>threshold, then
+                the answer is considered; otherwise, the answer is not considered
+        And it should return the accuracy of the model in percentage
+        """
+        assert mode in [0, 1, 2],\
+            "The model variable must be one of these values[0, 1, 2]"
+        assert scores.shape == (len(speakers), len(test_files)),\
+            "The dimensions of the input don't match"
+        accuracy = 0.
+        speakers = [sp.decode() for sp in speakers]
+        max_indices = np.argmax(scores, axis=0)
+        max_scores = np.max(scores, axis=0)
+        for idx, test_filename in enumerate(test_files):
+            test_filename = test_filename.decode() #convert from byte to string
+            actual_speaker = test_filename.split("/")[-1].split("_")[0]
+            predicted_speaker = speakers[max_indices[idx]]
+            #TODO: 
+            ########## JUST VERIFICATION ##########
+            if mode == 0:
+                if max_scores[idx] < threshold:
+                    continue
+                else:
+                    accuracy += 1.
+            ########## JUST RECOGNITION ##########
+            elif mode == 1:
+                #skip speakers outside the training
+                if actual_speaker not in speakers:
+                    continue
+                else:
+                    if predicted_speaker == actual_speaker:
+                        accuracy += 1.
+            ########## VERIFICATION & RECOGNITION ##########
+            elif mode == 2:
+                #skip speakers outside the training
+                if max_scores[idx] < threshold:
+                    continue
+                else:
+                    if predicted_speaker == actual_speaker:
+                        accuracy += 1.
+
+        return accuracy/len(test_files)
