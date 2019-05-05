08d6d9efb853ab53f2a1e0940d19c43b490ba7ed
anwarvic <mohamedanwarvic@gmail.com>
2019-05-05 09:55:56 +0200

    UBM now supports YAML configuration. It's tested only on 8/16/32/64... it needs more

--- UBM.py
+++ UBM.py
@@ -13,10 +13,10 @@ from model_interface import SidekitModel
 class UBM(SidekitModel):
     """Universal Background Model"""
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, conf_filepath):
+        super().__init__(conf_filepath)
         # Number of Guassian Distributions
-        self.NUM_GUASSIANS = 16
+        self.NUM_GAUSSIANS = self.conf['num_gaussians']
 
 
     def train(self, SAVE=True):
@@ -28,25 +28,26 @@ class UBM(SidekitModel):
         logging.info("Training...")
         ubm = sidekit.Mixture()
         # Set the model name
-        ubm.name = "ubm_{}.h5".format(self.NUM_GUASSIANS) 
+        ubm.name = "ubm_{}.h5".format(self.NUM_GAUSSIANS) 
         # Expectation-Maximization estimation of the Mixture parameters.
-        ubm.EM_split(features_server=server, #sidekit.FeaturesServer used to load data
-                     feature_list=train_list, # list of feature files to train the model
-                     distrib_nb=self.NUM_GUASSIANS, # final number of Gaussian distributions
-                     iterations=(1, 2, 2, 4, 4, 4, 4, 8, 8, 8, 8, 8, 8), # list of iteration number for each step of the learning process
-                     num_thread=self.NUM_THREADS, # number of thread to launch for parallel computing
-                     save_partial=False # if False, it only saves the last model
-                    )
-        # -> 2 iterations of EM with 2   distributions
-        # -> 2 iterations of EM with 4   distributions
-        # -> 4 iterations of EM with 8   distributions
-        # -> 4 iterations of EM with 16  distributions
-        # -> 4 iterations of EM with 32  distributions
-        # -> 4 iterations of EM with 64  distributions
-        # -> 8 iterations of EM with 128 distributions
-        # -> 8 iterations of EM with 256 distributions
-        # -> 8 iterations of EM with 512 distributions
-        # -> 8 iterations of EM with 1024 distributions
+        ubm.EM_split(
+            features_server=server, #sidekit.FeaturesServer used to load data
+            feature_list=train_list, #list of feature files to train the model
+            distrib_nb=self.NUM_GAUSSIANS, #number of Gaussian distributions
+            num_thread=self.NUM_THREADS, # number of parallel processes
+            save_partial=False, # if False, it only saves the last model
+            iterations=(1, 2, 2, 4, 4, 4, 4, 8, 8, 8, 8, 8, 8)
+            )
+            # -> 2 iterations of EM with 2    distributions
+            # -> 2 iterations of EM with 4    distributions
+            # -> 4 iterations of EM with 8    distributions
+            # -> 4 iterations of EM with 16   distributions
+            # -> 4 iterations of EM with 32   distributions
+            # -> 4 iterations of EM with 64   distributions
+            # -> 8 iterations of EM with 128  distributions
+            # -> 8 iterations of EM with 256  distributions
+            # -> 8 iterations of EM with 512  distributions
+            # -> 8 iterations of EM with 1024 distributions
         model_dir = os.path.join(self.BASE_DIR, "ubm")
         logging.info("Saving the model {} at {}".format(ubm.name, model_dir))
         ubm.write(os.path.join(model_dir, ubm.name))
@@ -67,7 +68,7 @@ class UBM(SidekitModel):
                                    )
         if SAVE:
             # Save the status of the enroll data
-            filename = "enroll_stat_{}.h5".format(self.NUM_GUASSIANS)
+            filename = "enroll_stat_{}.h5".format(self.NUM_GAUSSIANS)
             enroll_stat.write(os.path.join(self.BASE_DIR, "stat", filename))
 
 
@@ -80,11 +81,11 @@ class UBM(SidekitModel):
         test_ndx = sidekit.Ndx.read(os.path.join(self.BASE_DIR, "task", "test_ndx.h5"))
         # Read the UBM model
         ubm = sidekit.Mixture()
-        model_name = "ubm_{}.h5".format(self.NUM_GUASSIANS)
+        model_name = "ubm_{}.h5".format(self.NUM_GAUSSIANS)
         ubm.read(os.path.join(self.BASE_DIR, "ubm", model_name))
 
         ############################ Evaluating ###########################
-        filename = "enroll_stat_{}.h5".format(self.NUM_GUASSIANS)
+        filename = "enroll_stat_{}.h5".format(self.NUM_GAUSSIANS)
         enroll_stat = sidekit.StatServer.read(os.path.join(self.BASE_DIR, "stat", filename))
         # MAP adaptation of enrollment speaker models
         enroll_sv = enroll_stat.adapt_mean_map_multisession(ubm=ubm,
@@ -98,12 +99,12 @@ class UBM(SidekitModel):
                                              num_thread=self.NUM_THREADS
                                             )
         # Save the model's Score object
-        filename = "ubm_scores_{}.h5".format(self.NUM_GUASSIANS)
+        filename = "ubm_scores_{}.h5".format(self.NUM_GAUSSIANS)
         scores_gmm_ubm.write(os.path.join(self.BASE_DIR, "result", filename))
         
         #write Analysis
         if explain:
-            filename = "ubm_scores_explained_{}.txt".format(self.NUM_GUASSIANS)
+            filename = "ubm_scores_{}_explained.txt".format(self.NUM_GAUSSIANS)
             fout = open(os.path.join(self.BASE_DIR, "result", filename), "a")
             fout.truncate(0) #clear content
             modelset = list(scores_gmm_ubm.modelset)
@@ -112,7 +113,8 @@ class UBM(SidekitModel):
             for seg_idx, seg in enumerate(segset):
                 fout.write("Wav: {}\n".format(seg))
                 for speaker_idx, speaker in enumerate(modelset):
-                    fout.write("\tSpeaker {}:\t{}\n".format(speaker, scores[speaker_idx, seg_idx]))
+                    fout.write("\tSpeaker {}:\t{}\n"\
+                        .format(speaker, scores[speaker_idx, seg_idx]))
                 fout.write("\n")
             fout.close()
 
@@ -120,7 +122,7 @@ class UBM(SidekitModel):
 
     def plotDETcurve(self):
         # Read test scores
-        filename = "ubm_scores_{}.h5".format(self.NUM_GUASSIANS)
+        filename = "ubm_scores_{}.h5".format(self.NUM_GAUSSIANS)
         scores_dir = os.path.join(self.BASE_DIR, "result", filename)
         scores_gmm_ubm = sidekit.Scores.read(scores_dir)
         # Read the key
@@ -131,11 +133,17 @@ class UBM(SidekitModel):
         dp = sidekit.DetPlot(window_style='sre10', plot_title='Scores GMM-UBM')
         dp.set_system_from_scores(scores_gmm_ubm, key, sys_name='GMM-UBM')
         dp.create_figure()
-        dp.plot_rocch_det(0)
-        dp.plot_DR30_both(idx=0)
-        prior = sidekit.logit_effective_prior(0.01, 10, 1)
-        dp.plot_mindcf_point(prior, idx=0)
-        graphname = "DET_GMM_UBM_{}.png".format(self.NUM_GUASSIANS)
+        # DET type
+        if self.conf['DET_curve'] == "rocch":
+            dp.plot_rocch_det(idx=0)
+        elif self.conf['DET_curve'] == "steppy":
+            dp.plot_steppy_det(idx=0)
+        else:
+            raise NameError("Unsupported DET-curve-plotting method!!")
+        dp.plot_DR30_both(idx=0) #dotted line for Doddington's Rule
+        prior = sidekit.logit_effective_prior(0.001, 1, 1)
+        dp.plot_mindcf_point(prior, idx=0) #minimum dcf point
+        graphname = "DET_GMM_UBM_{}.png".format(self.NUM_GAUSSIANS)
         dp.__figure__.savefig(os.path.join(self.BASE_DIR, "result", graphname))
 
 
@@ -151,7 +159,7 @@ class UBM(SidekitModel):
         assert result_mode in [0, 1, 2], "Accuracy mode must be either 0, 1 or 2!!"
         import h5py
 
-        filename = "ubm_scores_{}.h5".format(self.NUM_GUASSIANS)
+        filename = "ubm_scores_{}.h5".format(self.NUM_GAUSSIANS)
         filepath = os.path.join(self.BASE_DIR, "result", filename)
         h5 = h5py.File(filepath, mode="r")
         modelset = list(h5["modelset"])
@@ -159,15 +167,18 @@ class UBM(SidekitModel):
         scores = np.array(h5["scores"])
         
         #get Accuracy
-        accuracy = super().getAccuracy(modelset, segest, scores, mode=result_mode, threshold=0)
+        accuracy = super().getAccuracy( modelset, segest, scores,
+                                        mode=result_mode,
+                                        threshold=0)
         return accuracy
 
 
 
 if __name__ == "__main__":
-    ubm = UBM()
+    conf_filename = "py3env/conf.yaml"
+    ubm = UBM(conf_filename)
     ubm.train()
     ubm.evaluate()
     ubm.plotDETcurve()
-    for mode in [0, 1, 2]:
+    for mode in [1, 2]:
         print( "Accuracy: {}%".format(ubm.getAccuracy(mode)*100) )
